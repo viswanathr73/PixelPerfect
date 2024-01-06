92bbcd0825131cfc53dce1385dd35bb293dee0ad
Viswanath <viswanathr73@gmail.com>
2024-01-06 17:26:21 +0530

    test

--- model/categoryModel.js
+++ model/categoryModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose'); // Erase if already required
+ const mongoose = require('mongoose'); // Erase if already required
 
 // Declare the Schema of the Mongo model
 var categorySchema = new mongoose.Schema({
